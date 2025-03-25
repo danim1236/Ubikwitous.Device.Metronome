2c7cf6d20e201aad8ead3a5e0d057c603c5e7787
Daniel Mendes <daniel@danim.com.br>
2025-03-25 16:24:26 -0700

    Wait for IDR / Sync Devices

--- python/ubikwitous_device_metronome.py
+++ python/ubikwitous_device_metronome.py
@@ -2,9 +2,10 @@
 
 import gi
 import time
+import socket
 import threading
 import queue
-import os
+import struct
 
 gi.require_version("Gst", "1.0")
 from gi.repository import Gst, GLib
@@ -12,53 +13,49 @@ from gi.repository import Gst, GLib
 Gst.init(None)
 
 SEGMENT_DURATION = 30
+UDP_PORT = 5005
 WRITE_QUEUE = 200
+
+
+def is_idr(data):
+    for i in range(len(data) - 4):
+        if data[i] == 0 and data[i+1] == 0 and data[i+2] == 0 and data[i+3] == 1:
+            nal = data[i+4] & 0x1F
+            if nal == 5:
+                return True
+    return False
 
 
 class DiskWriter(threading.Thread):
 
     def __init__(self, name):
-
         super().__init__(daemon=True)
-
         self.name = name
         self.queue = queue.Queue(WRITE_QUEUE)
         self.file = None
-        self.index = 0
         self.lock = threading.Lock()
 
-    def open_file(self):
-
+    def open_file(self, ts):
         with self.lock:
 
             if self.file:
                 self.file.close()
 
-            filename = f"{self.name}_{self.index:05d}.h264"
+            filename = f"{self.name}_{ts:.3f}.h264"
             print("OPEN", filename)
 
             self.file = open(filename, "wb")
-            self.index += 1
 
     def write(self, data):
-
         try:
             self.queue.put_nowait(data)
         except queue.Full:
             print(self.name, "disk queue overflow")
 
-    def rotate(self):
-
-        self.open_file()
-
     def run(self):
-
         while True:
-
             data = self.queue.get()
-
             with self.lock:
-
                 if self.file:
                     self.file.write(data)
 
@@ -70,6 +67,8 @@ class Camera:
         self.name = name
         self.url = url
 
+        self.pending_rotation = False
+
         self.writer = DiskWriter(name)
         self.writer.start()
 
@@ -80,7 +79,7 @@ class Camera:
         nvv4l2decoder !
         nvvideoconvert !
         nvv4l2h264enc iframeinterval=600 insert-sps-pps=true bitrate=4000000 !
-        h264parse !
+        h264parse config-interval=-1 !
         appsink name=sink emit-signals=true sync=false
         """
 
@@ -90,8 +89,6 @@ class Camera:
         self.appsink.connect("new-sample", self.on_sample)
 
     def start(self):
-
-        self.writer.open_file()
         self.pipeline.set_state(Gst.State.PLAYING)
 
     def on_sample(self, sink):
@@ -101,17 +98,28 @@ class Camera:
 
         ok, mapinfo = buf.map(Gst.MapFlags.READ)
 
-        if ok:
-            self.writer.write(mapinfo.data)
-            buf.unmap(mapinfo)
+        if not ok:
+            return Gst.FlowReturn.OK
+
+        data = mapinfo.data
+
+        if self.pending_rotation and is_idr(data):
+
+            pts = buf.pts / Gst.SECOND
+
+            self.writer.open_file(pts)
+
+            self.pending_rotation = False
+
+        self.writer.write(data)
+
+        buf.unmap(mapinfo)
 
         return Gst.FlowReturn.OK
 
-    def rotate(self):
-
-        self.writer.rotate()
-
     def force_keyframe(self):
+
+        self.pending_rotation = True
 
         event = Gst.Event.new_custom(
             Gst.EventType.CUSTOM_DOWNSTREAM,
@@ -121,52 +129,69 @@ class Camera:
         self.pipeline.send_event(event)
 
 
-class Scheduler(threading.Thread):
+class SyncReceiver(threading.Thread):
 
-    def __init__(self, cams):
-
+    def __init__(self, cameras):
         super().__init__(daemon=True)
-        self.cams = cams
+        self.cameras = cameras
 
     def run(self):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("", UDP_PORT))
+
+        while True:
+
+            data, _ = sock.recvfrom(1024)
+
+            if data == b"TICK":
+
+                print("SYNC TICK")
+
+                for c in self.cameras:
+                    c.force_keyframe()
+
+
+class SyncBroadcaster(threading.Thread):
+
+    def run(self):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
         while True:
 
             now = time.time()
 
-            boundary = (int(now / SEGMENT_DURATION) + 1) * SEGMENT_DURATION
+            next_boundary = (
+                int(now / SEGMENT_DURATION) + 1
+            ) * SEGMENT_DURATION
 
-            sleep = boundary - now
+            sleep = next_boundary - now
 
             time.sleep(sleep)
 
-            print("SEGMENT", boundary)
-
-            for c in self.cams:
-                c.force_keyframe()
-
-            time.sleep(0.05)
-
-            for c in self.cams:
-                c.rotate()
+            sock.sendto(b"TICK", ("255.255.255.255", UDP_PORT))
 
 
 def main():
 
-    cams = [
+    cameras = [
 
         Camera("cam1", "rtsp://127.0.0.1/stream1"),
         Camera("cam2", "rtsp://127.0.0.1/stream2"),
         Camera("cam3", "rtsp://127.0.0.1/stream3"),
-        Camera("cam4", "rtsp://127.0.0.1/stream3"),
+        Camera("cam4", "rtsp://127.0.0.1/stream4"),
 
     ]
 
-    for c in cams:
+    for c in cameras:
         c.start()
 
-    scheduler = Scheduler(cams)
-    scheduler.start()
+    SyncReceiver(cameras).start()
+
+    # apenas um Jetson deve executar isso
+    # SyncBroadcaster().start()
 
     loop = GLib.MainLoop()
     loop.run()
